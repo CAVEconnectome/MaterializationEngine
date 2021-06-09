3937426615ca8135190dde1e2cbbca081d2f0dc4
Forrest Collman <forrest.collman@gmail.com>
2021-06-09 13:19:57 -0700

    Bump version: 1.5.0 → 2.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2021, Derrick Brittain"
 author = "Derrick Brittain,  Forrest Collman, Sven Dorkenwald"
 
 # The full version, including alpha/beta/rc tags
-release = "1.5.0"
+release = "2.0.0"
 
 master_doc = 'index'
 
