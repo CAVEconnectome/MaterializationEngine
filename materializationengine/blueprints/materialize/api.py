d6a3b4e71e12684a3b90ef8f6219b1a4561aed1d
Forrest Collman <forrest.collman@gmail.com>
2021-06-28 16:31:05 -0700

    bumping reqs

--- materializationengine/blueprints/materialize/api.py
+++ materializationengine/blueprints/materialize/api.py
@@ -30,7 +30,8 @@ bulk_upload_parser.add_argument("schema", required=True, type=str)
 bulk_upload_parser.add_argument("materialized_ts", type=float)
 
 missing_chunk_parser = reqparse.RequestParser()
-missing_chunk_parser.add_argument("chunks", required=True, type=list, location="json")
+missing_chunk_parser.add_argument(
+    "chunks", required=True, type=list, location="json")
 missing_chunk_parser.add_argument(
     "column_mapping", required=True, type=dict, location="json"
 )
@@ -44,7 +45,8 @@ get_roots_parser.add_argument(
 )
 
 materialize_parser = reqparse.RequestParser()
-materialize_parser.add_argument("days_to_expire", required=True, default=None, type=int)
+materialize_parser.add_argument(
+    "days_to_expire", required=True, default=None, type=int)
 
 
 authorizations = {
@@ -68,7 +70,8 @@ def get_datastack_info(datastack_name: str) -> dict:
     INFOSERVICE_ENDPOINT = current_app.config["INFOSERVICE_ENDPOINT"]
     url = INFOSERVICE_ENDPOINT + f"/api/v2/datastack/full/{datastack_name}"
     try:
-        auth_header = {"Authorization": f"Bearer {current_app.config['AUTH_TOKEN']}"}
+        auth_header = {
+            "Authorization": f"Bearer {current_app.config['AUTH_TOKEN']}"}
         r = requests.get(url, headers=auth_header)
         r.raise_for_status()
         logging.info(url)
