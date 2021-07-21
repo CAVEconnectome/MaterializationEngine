5c1d49415e377f8b9ce50b1440e37607845edd52
Forrest Collman <forrest.collman@gmail.com>
2021-07-21 12:15:21 -0700

    Bump version: 2.5.3 → 2.5.4

--- materializationengine/blueprints/materialize/api.py
+++ materializationengine/blueprints/materialize/api.py
@@ -17,7 +17,7 @@ from sqlalchemy import MetaData, Table
 from sqlalchemy.engine.url import make_url
 from sqlalchemy.exc import NoSuchTableError
 
-__version__ = "2.5.3"
+__version__ = "2.5.4"
 
 
 bulk_upload_parser = reqparse.RequestParser()
--- materializationengine/views.py
+++ materializationengine/views.py
@@ -26,7 +26,7 @@ from middle_auth_client import (
     auth_requires_permission
 )
 
-__version__ = "2.5.3"
+__version__ = "2.5.4"
 
 views_bp = Blueprint("views", __name__, url_prefix="/materialize/views")
 
