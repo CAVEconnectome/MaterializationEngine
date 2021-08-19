acb689fd6b6cd65dfe5e8516c6b48651bdcdb0de
Forrest Collman <forrest.collman@gmail.com>
2021-08-19 07:40:12 -0700

    Bump version: 2.5.10 → 2.5.11

--- materializationengine/blueprints/materialize/api.py
+++ materializationengine/blueprints/materialize/api.py
@@ -17,7 +17,7 @@ from sqlalchemy import MetaData, Table
 from sqlalchemy.engine.url import make_url
 from sqlalchemy.exc import NoSuchTableError
 
-__version__ = "2.5.10"
+__version__ = "2.5.11"
 
 
 bulk_upload_parser = reqparse.RequestParser()
