968cd0d0543521a0b4b4026f26c45d9c33e4ac31
Derrick <dlbrittain@gmail.com>
2021-08-25 16:43:02 -0700

    fix(pcg): hardcode v1 pcg

--- materializationengine/chunkedgraph_gateway.py
+++ materializationengine/chunkedgraph_gateway.py
@@ -5,16 +5,26 @@ except:
 
 # TODO: put this in the infoservice
 
+v1_chunkedgraph_version_mapping = ["fly_v26", "fly_v31"]
+
+
+def check_pcg_mapping(table_id):
+    v1_pcg_set = set(v1_chunkedgraph_version_mapping)
+    table_id_set = set([table_id])
+    return bool((v1_pcg_set & table_id_set))
+
 
 class ChunkedGraphGateway:
     def __init__(self):
         self._cg = {}
 
     def init_pcg(self, table_id: str):
+        self.is_pcg_v1 = check_pcg_mapping(table_id)
+
         if table_id not in self._cg:
-            try:
+            if self.is_pcg_v1:
                 self._cg[table_id] = chunkedgraph.ChunkedGraph(table_id=table_id)
-            except TypeError:
+            else:
                 self._cg[table_id] = chunkedgraph.ChunkedGraph(graph_id=table_id)
 
         return self._cg[table_id]
