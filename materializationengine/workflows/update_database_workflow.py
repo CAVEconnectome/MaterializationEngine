d19ac631846b2a2c308f5c78d11e5cb3fe9ff909
dlbrittain <dlbrittain@gmail.com>
2021-10-26 13:41:38 -0700

    fix(live_update): ingest new annotations before root ids

--- materializationengine/workflows/update_database_workflow.py
+++ materializationengine/workflows/update_database_workflow.py
@@ -6,9 +6,10 @@ from celery import chain, chord
 from celery.utils.log import get_task_logger
 from materializationengine.blueprints.materialize.api import get_datastack_info
 from materializationengine.celery_init import celery
-from materializationengine.shared_tasks import (generate_chunked_model_ids, fin,
-                                                workflow_complete,
-                                                get_materialization_info)
+from materializationengine.shared_tasks import (fin,
+                                                generate_chunked_model_ids,
+                                                get_materialization_info,
+                                                workflow_complete)
 from materializationengine.utils import get_config_param
 from materializationengine.workflows.ingest_new_annotations import \
     ingest_new_annotations_workflow
@@ -16,7 +17,6 @@ from materializationengine.workflows.update_root_ids import (
     get_expired_root_ids, update_root_ids_workflow)
 
 celery_logger = get_task_logger(__name__)
-
 
 
 @celery.task(name="process:run_periodic_database_update")
@@ -28,14 +28,14 @@ def run_periodic_database_update() -> None:
 
     """
     try:
-        datastacks = json.loads(os.environ['DATASTACKS'])
+        datastacks = json.loads(os.environ["DATASTACKS"])
     except:
-        datastacks = get_config_param('DATASTACKS')
+        datastacks = get_config_param("DATASTACKS")
 
     for datastack in datastacks:
         try:
             celery_logger.info(f"Start periodic database update job for {datastack}")
-            datastack_info = get_datastack_info(datastack)  
+            datastack_info = get_datastack_info(datastack)
             task = update_database_workflow.s(datastack_info)
             task.apply_async()
         except Exception as e:
@@ -47,7 +47,7 @@ def run_periodic_database_update() -> None:
 @celery.task(name="process:update_database_workflow")
 def update_database_workflow(datastack_info: dict):
     """Updates 'live' database:
-        - Find all annotations with missing segmentation rows 
+        - Find all annotations with missing segmentation rows
         and lookup supervoxel_id and root_id
         - Lookup all expired root_ids and update them
 
@@ -57,9 +57,11 @@ def update_database_workflow(datastack_info: dict):
     """
     materialization_time_stamp = datetime.datetime.utcnow()
 
-    mat_info = get_materialization_info(datastack_info=datastack_info, 
-                                        analysis_version=None, 
-                                        materialization_time_stamp=materialization_time_stamp)
+    mat_info = get_materialization_info(
+        datastack_info=datastack_info,
+        analysis_version=None,
+        materialization_time_stamp=materialization_time_stamp,
+    )
     celery_logger.info(mat_info)
 
     update_live_database_workflow = []
@@ -69,31 +71,33 @@ def update_database_workflow(datastack_info: dict):
     for mat_metadata in mat_info:
         annotation_chunks = generate_chunked_model_ids(mat_metadata)
         chunked_roots = get_expired_root_ids(mat_metadata)
-        if chunked_roots:
-            update_expired_roots_workflow = update_root_ids_workflow(
-                                                mat_metadata, chunked_roots)
-
-        if mat_metadata['row_count'] < 1_000_000:
+        if mat_metadata["row_count"] < 1_000_000:
             new_annotations = True
             new_annotation_workflow = ingest_new_annotations_workflow(
-                mat_metadata, annotation_chunks)
+                mat_metadata, annotation_chunks
+            )
         else:
             new_annotations = None
 
         if chunked_roots:
+            update_expired_roots_workflow = update_root_ids_workflow(
+                mat_metadata, chunked_roots
+            )
             if new_annotations:
                 ingest_and_update_root_ids_workflow = chain(
-                new_annotation_workflow, update_expired_roots_workflow)
-                update_live_database_workflow.append(ingest_and_update_root_ids_workflow)
+                    new_annotation_workflow, update_expired_roots_workflow
+                )
+                update_live_database_workflow.append(
+                    ingest_and_update_root_ids_workflow
+                )
             else:
                 update_live_database_workflow.append(update_expired_roots_workflow)
+        elif new_annotations:
+            update_live_database_workflow.append(new_annotation_workflow)
         else:
-            if new_annotations:
-                update_live_database_workflow.append(new_annotation_workflow)
-            else:
-                return "Nothing to update"
+            return "Nothing to update"
 
     run_update_database_workflow = chain(
-         chord(update_live_database_workflow, workflow_complete.si("update_root_ids")),
+        chord(update_live_database_workflow, workflow_complete.si("update_root_ids")),
     )
     run_update_database_workflow.apply_async()
