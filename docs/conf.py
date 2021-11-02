74f6e607c81f60bdfac4c45fc7af9325d60ea886
Derrick <dlbrittain@gmail.com>
2021-11-01 18:18:02 -0700

    Bump version: 2.8.3 → 2.8.4

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2021, Derrick Brittain"
 author = "Derrick Brittain,  Forrest Collman, Sven Dorkenwald"
 
 # The full version, including alpha/beta/rc tags
-release = "2.8.3"
+release = "2.8.4"
 
 master_doc = 'index'
 
