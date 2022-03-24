d98980bfde92e307cee020bb969eff3b82978065
Derrick Brittain <dlbrittain@gmail.com>
2022-03-24 12:38:14 -0700

    fix: paramerterize graceful shutdown script to handle different worker and queue names

--- gracefully_shutdown_celery.sh
+++ gracefully_shutdown_celery.sh
@@ -6,19 +6,19 @@ export CELERY_BROKER_URL=redis://:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}/
 
 # prevent the worker accepting new tasks
 echo "*** EVENT: Worker not accepting new tasks"
-celery control --broker $CELERY_BROKER_URL --destination worker.process@$HOSTNAME cancel_consumer process
+celery control --destination $WORKER_NAME@$HOSTNAME cancel_consumer $QUEUE_NAME
 sleep 5
 # loop until all active task are finished
 echo "*** EVENT: Waiting for remaining tasks to finish"
 while : ; do
-    IS_ACTIVE=$( celery inspect --broker $CELERY_BROKER_URL --destination worker.process@$HOSTNAME --json active | python3 -c "import json; active_tasks = json.loads(input())['worker.process@$HOSTNAME']; print(len(active_tasks))")
+    IS_ACTIVE=$( celery inspect --destination $WORKER_NAME@$HOSTNAME --json active | python3 -c "import json; active_tasks = json.loads(input())['$WORKER_NAME@$HOSTNAME']; print(len(active_tasks))")
     if (( $IS_ACTIVE > 0 ))
     then
         sleep 10
         dt=$(date '+%d/%m/%Y %H:%M:%S');
-        echo -n -e "*** STATUS: Number of tasks being run on worker.process@${HOSTNAME} is: ${IS_ACTIVE}. Current time: ${dt}, waiting...\r"
+        echo -n -e "*** STATUS: Number of tasks being run on ${WORKER_NAME}@${HOSTNAME} is: ${IS_ACTIVE}. Current time: ${dt}, waiting...\r"
     else
-        echo -n -e "*** STATUS: Number of tasks being run on worker.process@${HOSTNAME} is: ${IS_ACTIVE}. Current time: ${dt}, stopping...\r"
+        echo -n -e "*** STATUS: Number of tasks being run on ${WORKER_NAME}@${HOSTNAME} is: ${IS_ACTIVE}. Current time: ${dt}, stopping...\r"
         echo "*** EVENT: No active running tasks found, shutting down"
         break
     fi
