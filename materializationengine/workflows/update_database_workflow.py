7c8f2f65fdd567c6412188756b579fb675016440
Derrick Brittain <dlbrittain@gmail.com>
2022-04-01 14:41:38 -0700

    fix: remove late ack to repvent duplicate tasks; increase lock timeout

--- materializationengine/workflows/update_database_workflow.py
+++ materializationengine/workflows/update_database_workflow.py
@@ -51,9 +51,8 @@ def run_periodic_database_update() -> None:
 @celery.task(
     bind=True,
     base=LockedTask,
-    timeout=int(60 * 60 * 1.5),  # Task locked for 1.5 hours
+    timeout=int(60 * 60 * 24 * 7),  # Task locked for 1 week
     name="workflow:update_database_workflow",
-    acks_late=True,
 )
 def update_database_workflow(self, datastack_info: dict, **kwargs):
     """Updates 'live' database:
