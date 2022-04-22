bd56ba73ffd19a0623c89d8cb6720fda034a777b
Forrest Collman <forrest.collman@gmail.com>
2022-04-22 13:42:40 -0700

    fixing warning concatenation

--- materializationengine/blueprints/client/api.py
+++ materializationengine/blueprints/client/api.py
@@ -672,7 +672,7 @@ class LiveTableQuery(Resource):
                 "return_pandas=true is deprecated and may convert columns with nulls to floats, Please upgrade CAVEclient to >XXX with pip install -U caveclient"
             )
         if len(warnings) > 0:
-            headers = {"Warning": "\n".join(warnings)}
+            headers = {"Warning": ". ".join(warnings)}
         if args["return_pyarrow"]:
             context = pa.default_serialization_context()
             serialized = context.serialize(df).to_buffer().to_pybytes()
@@ -808,7 +808,7 @@ class FrozenTableQuery(Resource):
                 please upgrade CAVEclient to >XXX with pip install -U caveclient"
             )
         if len(warnings) > 0:
-            headers = {"Warning": "\n".join(warnings)}
+            headers = {"Warning": ". ".join(warnings)}
 
         if args["return_pyarrow"]:
             context = pa.default_serialization_context()
@@ -923,7 +923,7 @@ class FrozenQuery(Resource):
                 please upgrade CAVEclient to >XXX with pip install -U caveclient"
             )
         if len(warnings) > 0:
-            headers = {"Warning": "\n".join(warnings)}
+            headers = {"Warning": ". ".join(warnings)}
 
         if args["return_pyarrow"]:
             context = pa.default_serialization_context()
