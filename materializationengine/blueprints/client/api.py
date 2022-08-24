560eb18a98f1391467694a0aa2fc53d2105620ed
Derrick Brittain <dlbrittain@gmail.com>
2022-08-24 14:15:45 -0700

    fix: api version was wrong

--- materializationengine/blueprints/client/api.py
+++ materializationengine/blueprints/client/api.py
@@ -24,7 +24,7 @@ from materializationengine.schemas import AnalysisTableSchema, AnalysisVersionSc
 from middle_auth_client import auth_requires_permission
 from materializationengine.blueprints.client.datastack import validate_datastack
 
-__version__ = "4.0.20"
+__version__ = "4.0.22"
 
 
 authorizations = {
