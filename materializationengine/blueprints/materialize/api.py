da8d612d65c99802e6960af4ed7350dfafd02f4b
Forrest Collman <forrest.collman@gmail.com>
2022-10-25 09:54:57 -0700

    Bump version: 4.3.1 → 4.3.2

--- materializationengine/blueprints/materialize/api.py
+++ materializationengine/blueprints/materialize/api.py
@@ -25,7 +25,7 @@ from sqlalchemy.exc import NoSuchTableError
 
 from materializationengine.blueprints.materialize.schemas import VirtualVersionSchema
 
-__version__ = "4.3.1"
+__version__ = "4.3.2"
 
 
 bulk_upload_parser = reqparse.RequestParser()
