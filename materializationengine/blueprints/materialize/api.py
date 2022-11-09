6c0d84f2e5dedbbf1c4ed30b7b13b2d4c2fa771d
Forrest Collman <forrest.collman@gmail.com>
2022-11-08 17:32:22 -0800

    modifying injest pipeline to write permissions on table

--- materializationengine/blueprints/materialize/api.py
+++ materializationengine/blueprints/materialize/api.py
@@ -18,10 +18,11 @@ from materializationengine.info_client import (
 )
 from dynamicannotationdb.models import AnalysisVersion
 from materializationengine.schemas import AnalysisTableSchema, AnalysisVersionSchema
-from middle_auth_client import auth_requires_admin
+from middle_auth_client import auth_requires_admin, auth_requires_permission
 from sqlalchemy import MetaData, Table
 from sqlalchemy.engine.url import make_url
 from sqlalchemy.exc import NoSuchTableError
+from materializationengine.utils import check_write_permission
 
 from materializationengine.blueprints.materialize.schemas import VirtualVersionSchema
 
@@ -141,7 +142,7 @@ class ProcessNewAnnotationsResource(Resource):
 )
 class ProcessNewAnnotationsTableResource(Resource):
     @reset_auth
-    @auth_requires_admin
+    @auth_requires_permission("edit", table_arg="datastack_name")
     @mat_bp.doc("process new annotations workflow", security="apikey")
     def post(self, datastack_name: str, table_name: str):
         """Process newly added annotations and lookup segmentation data
@@ -155,6 +156,9 @@ class ProcessNewAnnotationsTableResource(Resource):
         )
 
         datastack_info = get_datastack_info(datastack_name)
+        db = dynamic_annotation_cache[datastack_info["aligned_volume"]["name"]]
+        check_write_permission(db, table_name)
+
         process_new_annotations_workflow.s(
             datastack_info, table_name=table_name
         ).apply_async()
