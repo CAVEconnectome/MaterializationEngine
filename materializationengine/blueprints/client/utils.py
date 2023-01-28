a0a35487542d2052a26eb577a87ca79992da615b
Forrest Collman <forrest.collman@gmail.com>
2023-01-28 09:39:56 -0800

    adding utils file

---
+++ materializationengine/blueprints/client/utils.py
@@ -0,0 +1,13 @@
+def add_warnings_to_headers(headers, warnings):
+    if len(warnings) > 0:
+        headers["Warning"] = "\n".join(warnings)
+    return headers
+
+
+def update_notice_text_warnings(ann_md, warnings):
+    notice_text = ann_md.get("notice_text", None)
+    if notice_text is not None:
+        msg = f"Table Owner Warning: {notice_text}"
+        warnings.append(msg)
+
+    return warnings
