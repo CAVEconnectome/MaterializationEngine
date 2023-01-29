05d66a9216ffce449db380ad8bc63710574b354a
Forrest Collman <forrest.collman@gmail.com>
2023-01-29 13:00:44 -0800

    switching to body of request

--- materializationengine/blueprints/materialize/schemas.py
+++ materializationengine/blueprints/materialize/schemas.py
@@ -1,6 +1,8 @@
 from marshmallow import fields, Schema
 
-
+class AnnotationIDListSchema(Schema):
+    annotation_ids = fields.List(fields.Int, required=True)
+    
 class VirtualVersionSchema(Schema):
     datastack_name = fields.Str()
     target_version = fields.Integer()
