a7a555472850d0e2297eb355ec5407d8dd3e2b80
Forrest Collman <forrest.collman@gmail.com>
2023-04-06 20:21:37 -0700

    Bump version: 4.9.4 → 4.10.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Derrick Brittain"
 author = "Derrick Brittain,  Forrest Collman, Sven Dorkenwald"
 
 # The full version, including alpha/beta/rc tags
-release = "4.9.4"
+release = "4.10.0"
 
 master_doc = "index"
 
--- materializationengine/blueprints/materialize/api.py
+++ materializationengine/blueprints/materialize/api.py
@@ -31,7 +31,7 @@ from materializationengine.blueprints.materialize.schemas import (
 )
 
 
-__version__ = "4.9.4"
+__version__ = "4.10.0"
 
 
 bulk_upload_parser = reqparse.RequestParser()
