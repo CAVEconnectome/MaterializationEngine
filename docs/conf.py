7dc2234350878c9f75911f321c0d422346a96914
Forrest Collman <forrest.collman@gmail.com>
2023-04-23 18:48:54 -0700

    Bump version: 4.12.3 → 4.12.4

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Derrick Brittain"
 author = "Derrick Brittain,  Forrest Collman, Sven Dorkenwald"
 
 # The full version, including alpha/beta/rc tags
-release = "4.12.3"
+release = "4.12.4"
 
 master_doc = "index"
 
