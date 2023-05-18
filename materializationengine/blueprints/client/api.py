4cad7d902d787a3cc226ed4c26843b40fe0ec172
Forrest Collman <forrest.collman@gmail.com>
2023-05-18 05:49:24 -0700

    Bump version: 4.14.13 → 4.15.0

--- materializationengine/blueprints/client/api.py
+++ materializationengine/blueprints/client/api.py
@@ -30,7 +30,7 @@ from materializationengine.blueprints.client.datastack import validate_datastack
 
 
 
-__version__ = "4.14.13"
+__version__ = "4.15.0"
 
 
 authorizations = {
--- materializationengine/blueprints/materialize/api.py
+++ materializationengine/blueprints/materialize/api.py
@@ -31,7 +31,7 @@ from materializationengine.blueprints.materialize.schemas import (
 )
 
 
-__version__ = "4.14.13"
+__version__ = "4.15.0"
 
 
 bulk_upload_parser = reqparse.RequestParser()
