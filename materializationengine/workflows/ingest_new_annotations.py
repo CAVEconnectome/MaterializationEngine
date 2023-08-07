a8fbeb4ce12b0d2120fac6683994ccd463ed9afc
Derrick Brittain <dlbrittain@gmail.com>
2023-08-07 11:18:21 -0700

    fix: can use create seg table as task

--- materializationengine/workflows/ingest_new_annotations.py
+++ materializationengine/workflows/ingest_new_annotations.py
@@ -102,7 +102,9 @@ def ingest_table_svids(
     mat_metadata = mat_info[0]  # only one entry for a single table
     table_created = create_missing_segmentation_table(mat_metadata)
     if table_created:
-        celery_logger.info(f'Table created: {mat_metadata.get("segmentation_table_name")}')
+        celery_logger.info(
+            f'Table created: {mat_metadata.get("segmentation_table_name")}'
+        )
     if annotation_ids:
         ingest_workflow = ingest_new_annotations.si(
             None, mat_metadata, annotation_ids, lookup_root_ids=False
@@ -399,6 +401,7 @@ def ingest_new_annotations_workflow(mat_metadata: dict):
         return fin.si()
 
 
+@celery.task(name="workflow:create_missing_segmentation_table", acks_late=True)
 def create_missing_segmentation_table(mat_metadata: dict) -> bool:
     """Create missing segmentation tables associated with an annotation table if it
     does not already exist.
