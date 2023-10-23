0ef975fac5b36a5958b108fc658f3bd5c187028a
Forrest Collman <forrest.collman@gmail.com>
2023-10-24 08:09:32 +0900

    Bump version: 4.20.2 → 4.21.0

--- materializationengine/blueprints/materialize/api.py
+++ materializationengine/blueprints/materialize/api.py
@@ -32,7 +32,7 @@ from materializationengine.blueprints.materialize.schemas import (
 )
 
 
-__version__ = "4.20.2"
+__version__ = "4.21.0"
 
 
 bulk_upload_parser = reqparse.RequestParser()
--- materializationengine/views.py
+++ materializationengine/views.py
@@ -51,7 +51,7 @@ from nglui.statebuilder.helpers import package_state, make_point_statebuilder
 import caveclient
 
 
-__version__ = "4.20.2"
+__version__ = "4.21.0"
 
 views_bp = Blueprint("views", __name__, url_prefix="/materialize/views")
 
