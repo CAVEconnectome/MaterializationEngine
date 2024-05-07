893fef34586e00977e65fd5e68a3a386c4098127
Forrest Collman <forrest.collman@gmail.com>
2024-05-07 11:35:01 -0700

    moving import

--- materializationengine/taskqueue_worker.py
+++ materializationengine/taskqueue_worker.py
@@ -1,14 +1,14 @@
 from taskqueue import TaskQueue
-from materializationengine.workflows.spatial_lookup import (
-    process_spatially_chunked_svids_func,
-)
-
 from materializationengine.app import create_app
 from materializationengine.utils import get_config_param
 
 application = create_app()
 
 with application.app_context():
+    from materializationengine.workflows.spatial_lookup import (
+        process_spatially_chunked_svids_func,
+    )
+
     tq = TaskQueue(get_config_param("TASKQUEUE_QURL"))
     tq.poll(
         lease_seconds=int(
