021b4aeb7e44865448ed7c63063066116fdf8b12
Forrest Collman <forrest.collman@gmail.com>
2024-05-23 18:23:26 -0700

    Bump version: 4.25.4 → 4.26.0

--- materializationengine/blueprints/client/api2.py
+++ materializationengine/blueprints/client/api2.py
@@ -53,7 +53,7 @@ from materializationengine.schemas import AnalysisTableSchema, AnalysisVersionSc
 from materializationengine.blueprints.client.utils import update_notice_text_warnings
 
 
-__version__ = "4.25.4"
+__version__ = "4.26.0"
 
 
 authorizations = {
