e4001e93c61ad261f9fa2f802acdad2ba94bad40
Derrick Brittain <dlbrittain@gmail.com>
2024-06-14 10:30:41 -0400

    fix: add admin protection to migration code

--- materializationengine/admin.py
+++ materializationengine/admin.py
@@ -1,8 +1,8 @@
-from flask_admin import Admin, BaseView, expose
+from flask_admin import Admin, AdminIndexView, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
 from dynamicannotationdb.models import AnalysisVersion, AnalysisTable
-from flask import request
-from flask import redirect, url_for, request, current_app
+from flask import request, current_app, g, render_template
+from middle_auth_client import auth_required
 from dynamicannotationdb.migration import DynamicMigration, run_alembic_migration
 from sqlalchemy.engine.url import make_url
 from materializationengine.info_client import (
@@ -11,13 +11,26 @@ from materializationengine.info_client import (
 )
 
 
+class MatAdminIndexView(AdminIndexView):
+    @expose("/", methods=["GET"])
+    @auth_required
+    def index(self):
+        return super(MatAdminIndexView, self).index()
+
+    def is_accessible(self):
+        @auth_required
+        def helper():
+            return True
+
+        return helper()
+
+
 def setup_admin(app, db):
-    admin = Admin(app, name="materializationengine")
+    admin = Admin(app, name="materializationengine", index_view=MatAdminIndexView())
     admin.add_view(MigrationView(name="Migration"))
     admin.add_view(ModelView(AnalysisVersion, db.session))
     admin.add_view(ModelView(AnalysisTable, db.session))
     return admin
-
 
 
 def get_allowed_aligned_volumes():
@@ -29,36 +42,64 @@ def get_allowed_aligned_volumes():
             aligned_volumes.append(aligned_volume_name)
     return aligned_volumes
 
+
 class MigrationView(BaseView):
-    @expose('/')
+    def is_accessible(self):
+        @auth_required
+        def helper():
+            return True
+
+        return helper() and g.get("auth_user", {}).get("admin", False)
+
+    def inaccessible_callback(self, name, **kwargs):
+        return render_template("admin/403.html"), 403
+
+    @expose("/")
+    @auth_required
     def index(self):
         aligned_volumes = get_allowed_aligned_volumes()
-        return self.render('admin/migration.html', aligned_volumes=aligned_volumes)
+        return self.render("admin/migration.html", aligned_volumes=aligned_volumes)
 
-    @expose('/migrate_static_schemas', methods=['POST'])
+    @expose("/migrate_static_schemas", methods=["POST"])
+    @auth_required
     def migrate_static_schemas(self):
-        sql_url = request.form.get('sql_url')
-        aligned_volume = request.form.get('aligned_volume')
+        sql_url = request.form.get("sql_url")
+        aligned_volume = request.form.get("aligned_volume")
         sql_base_uri = sql_url.rpartition("/")[0]
         sql_uri = make_url(f"{sql_base_uri}/{aligned_volume}")
         migrator = run_alembic_migration(str(sql_uri))
-        return self.render('admin/migration.html', message=migrator, aligned_volumes=get_allowed_aligned_volumes())
+        return self.render(
+            "admin/migration.html",
+            message=migrator,
+            aligned_volumes=get_allowed_aligned_volumes(),
+        )
 
-    @expose('/migrate_annotation_schemas', methods=['POST'])
+    @expose("/migrate_annotation_schemas", methods=["POST"])
+    @auth_required
     def migrate_annotation_schemas(self):
-        sql_url = request.form.get('sql_url')
-        aligned_volume = request.form.get('aligned_volume')
-        dry_run = request.form.get('dry_run') == 'true'
+        sql_url = request.form.get("sql_url")
+        aligned_volume = request.form.get("aligned_volume")
+        dry_run = request.form.get("dry_run") == "true"
         migrator = DynamicMigration(sql_url, aligned_volume)
         migrations = migrator.upgrade_annotation_models(dry_run=dry_run)
-        return self.render('admin/migration.html', message=migrations, aligned_volumes=get_allowed_aligned_volumes())
+        return self.render(
+            "admin/migration.html",
+            message=migrations,
+            aligned_volumes=get_allowed_aligned_volumes(),
+        )
 
-    @expose('/migrate_foreign_key_constraints', methods=['POST'])
+    @expose("/migrate_foreign_key_constraints", methods=["POST"])
+    @auth_required
     def migrate_foreign_key_constraints(self):
-        sql_url = request.form.get('sql_url')
-        aligned_volume = request.form.get('aligned_volume')
-        dry_run = request.form.get('dry_run') == 'true'
+        sql_url = request.form.get("sql_url")
+        aligned_volume = request.form.get("aligned_volume")
+        dry_run = request.form.get("dry_run") == "true"
         migrator = DynamicMigration(sql_url, aligned_volume)
-        fkey_constraint_mapping = migrator.apply_cascade_option_to_tables(dry_run=dry_run)
-        return self.render('admin/migration.html', message=fkey_constraint_mapping, aligned_volumes=get_allowed_aligned_volumes())
-
+        fkey_constraint_mapping = migrator.apply_cascade_option_to_tables(
+            dry_run=dry_run
+        )
+        return self.render(
+            "admin/migration.html",
+            message=fkey_constraint_mapping,
+            aligned_volumes=get_allowed_aligned_volumes(),
+        )
