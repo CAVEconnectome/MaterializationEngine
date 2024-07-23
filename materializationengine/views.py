567781232ea998e7b6221ba4b2d7961a4329f8b6
Forrest Collman <forrest.collman@gmail.com>
2024-07-23 13:39:33 -0700

    Bump version: 4.30.1 → 4.31.0

--- materializationengine/views.py
+++ materializationengine/views.py
@@ -55,7 +55,7 @@ from nglui.statebuilder.helpers import package_state, make_point_statebuilder
 import caveclient
 
 
-__version__ = "4.30.1"
+__version__ = "4.31.0"
 
 views_bp = Blueprint("views", __name__, url_prefix="/materialize/views")
 
