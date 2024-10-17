2fe4ddc75b32b5bd197c25127db9dca68b409deb
Derrick Brittain <dlbrittain@gmail.com>
2024-10-17 12:45:44 -0700

    feat: extract migration logic for kube job (#167)
    * feat: extract migration logic for kube job
    
    * fix: removed extra rpartition from uri string
    
    * fix: move migration code
    
    * feat: add flask command
    
    * fix: app context issues

--- materializationengine/app.py
+++ materializationengine/app.py
@@ -21,7 +21,7 @@ from materializationengine.schemas import ma
 from materializationengine.utils import get_instance_folder_path
 from materializationengine.views import views_bp
 from materializationengine.limiter import limiter
-
+from materializationengine.migrate import migrator
 db = SQLAlchemy(model_class=Base)
 
 
@@ -70,6 +70,9 @@ def create_app(config_name: str = None):
 
     db.init_app(app)
     ma.init_app(app)
+
+    # add the AppGroup to the app
+    app.cli.add_command(migrator)
 
     with app.app_context():
         api = Api(
--- materializationengine/migrate.py
+++ materializationengine/migrate.py
@@ -1,6 +1,9 @@
+import logging
+
 import click
 from dynamicannotationdb.migration import DynamicMigration, run_alembic_migration
-from run import application
+from flask import current_app
+from flask.cli import AppGroup
 from sqlalchemy.engine.url import make_url
 
 from materializationengine.info_client import (
@@ -8,27 +11,52 @@ from materializationengine.info_client import (
     get_relevant_datastack_info,
 )
 
+logger = logging.getLogger(__name__)
+
+migrator = AppGroup("migrator")
+
 
 def get_allowed_aligned_volumes():
-    with application.app_context():
+    with current_app.app_context():
         datastacks = get_datastacks()
         aligned_volumes = []
         for datastack in datastacks:
             aligned_volume_name, pcg_table_name = get_relevant_datastack_info(datastack)
             aligned_volumes.append(aligned_volume_name)
-    return aligned_volumes
+        return aligned_volumes
 
 
-@click.group(help="Migration tools")
-def migrator():
-    pass
+def migrate_static_schemas(sql_url: str, aligned_volume: str):
+    sql_base_uri = sql_url.rpartition("/")[0]
+    logging.info(sql_base_uri)
+    sql_uri = make_url(f"{sql_base_uri}/{aligned_volume}")
+    return run_alembic_migration(str(sql_uri))
+
+
+@migrator.command()
+def auto_migrate():
+    sql_uri = current_app.config["SQLALCHEMY_DATABASE_URI"]
+    aligned_volumes = get_allowed_aligned_volumes()
+    for aligned_volume in aligned_volumes:
+        logger.info(f"Migrating {aligned_volume}")
+        migration_status = migrate_static_schemas(sql_uri, aligned_volume)
+        logger.info(f"Migrated {aligned_volume} with {migration_status}")
+
+
+def validate_aligned_volume(ctx, param, value):
+    if value is None:
+        return None
+    allowed_volumes = get_allowed_aligned_volumes()
+    if value not in allowed_volumes:
+        raise click.BadParameter(f"Must be one of: {', '.join(allowed_volumes)}")
+    return value
 
 
 @migrator.command(help="Migrate metadata schemas")
 @click.option(
     "--sql_url",
     prompt=True,
-    default=lambda: application.config["SQLALCHEMY_DATABASE_URI"],
+    default=lambda: current_app.config["SQLALCHEMY_DATABASE_URI"],
     show_default="SQL URL from config",
 )
 @click.option(
@@ -36,20 +64,18 @@ def migrator():
     "--aligned_volume",
     prompt="Target Aligned Volume",
     help="Aligned Volume database to migrate",
-    type=click.Choice(get_allowed_aligned_volumes()),
+    callback=validate_aligned_volume,
 )
-def migrate_static_schemas(sql_url: str, aligned_volume: str):
-    sql_base_uri = sql_url.rpartition("/")[0]
-    sql_uri = make_url(f"{sql_base_uri}/{aligned_volume}")
-    migrator = run_alembic_migration(str(sql_uri))
-    click.echo(migrator)
+def migrate(sql_url: str, aligned_volume: str):
+    migration_status = migrate_static_schemas(sql_url, aligned_volume)
+    click.echo(migration_status)
 
 
 @migrator.command(help="Migrate dynamic annotation schemas")
 @click.option(
     "--sql_url",
     prompt=True,
-    default=lambda: application.config["SQLALCHEMY_DATABASE_URI"],
+    default=lambda: current_app.config["SQLALCHEMY_DATABASE_URI"],
     show_default="SQL URL from config",
 )
 @click.option(
@@ -57,7 +83,7 @@ def migrate_static_schemas(sql_url: str, aligned_volume: str):
     "--aligned_volume",
     prompt="Target Aligned Volume",
     help="Aligned Volume database to migrate",
-    type=click.Choice(get_allowed_aligned_volumes()),
+    callback=validate_aligned_volume,
 )
 @click.option(
     "--dry_run", prompt="Dry Run", help="Test migration before running", default=True
@@ -72,7 +98,7 @@ def migrate_annotation_schemas(sql_url: str, aligned_volume: str, dry_run: bool 
 @click.option(
     "--sql_url",
     prompt=True,
-    default=lambda: application.config["SQLALCHEMY_DATABASE_URI"],
+    default=lambda: current_app.config["SQLALCHEMY_DATABASE_URI"],
     show_default="SQL URL from config",
 )
 @click.option(
@@ -80,16 +106,14 @@ def migrate_annotation_schemas(sql_url: str, aligned_volume: str, dry_run: bool 
     "--aligned_volume",
     prompt="Target Aligned Volume",
     help="Aligned Volume database to migrate",
-    type=click.Choice(get_allowed_aligned_volumes()),
+    callback=validate_aligned_volume,
 )
 @click.option(
     "--dry_run", prompt="Dry Run", help="Test migration before running", default=True
 )
-def migrate_foreign_key_constraints(sql_url: str, aligned_volume: str, dry_run: bool = True):
+def migrate_foreign_key_constraints(
+    sql_url: str, aligned_volume: str, dry_run: bool = True
+):
     migrator = DynamicMigration(sql_url, aligned_volume)
     fkey_constraint_mapping = migrator.apply_cascade_option_to_tables(dry_run=dry_run)
     click.echo(fkey_constraint_mapping)
-
-
-if __name__ == "__main__":
-    migrator()
