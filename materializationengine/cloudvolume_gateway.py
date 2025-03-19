63f1ff5a67f72afe7143d1db5f876e2829858b33
dlbrittain <dlbrittain@gmail.com>
2025-03-18 19:20:07 -0700

    feat: improve cv cache

--- materializationengine/cloudvolume_gateway.py
+++ materializationengine/cloudvolume_gateway.py
@@ -5,11 +5,12 @@ import os
 class CloudVolumeGateway:
     """A class to manage cloudvolume clients and cache them for reuse."""
 
-    def __init__(self, lru_bytes: int = 0):
+    def __init__(self, lru_bytes: int = 1024 * 1024 * 64 ):
         self._cv_clients = {}
         self._lru_bytes = lru_bytes
 
-    def get_cv(self, seg_source: str, mip_level: int = 0) -> cloudvolume.CloudVolume:
+
+    def get_cv(self, seg_source: str, mip_level: int = 0, use_green_threads: bool = False) -> cloudvolume.CloudVolume:
         """A function to get a cloudvolume client for a given source.
 
         Args:
@@ -20,14 +21,18 @@ class CloudVolumeGateway:
             cloudvolume.CloudVolume: The cloudvolume client.
         """
         seg_source_key = seg_source.split("/")[-1]
+        if use_green_threads:
+            seg_source_key = f"{seg_source}_green_threads"
+        
+        
         return (
-            self._get_cv_client(seg_source, seg_source_key, mip_level)
+            self._get_cv_client(seg_source, seg_source_key, mip_level, use_green_threads)
             if seg_source_key not in self._cv_clients
             else self._cv_clients[seg_source_key]
         )
 
     def _get_cv_client(
-        self, seg_source: str, seg_source_key: str, mip_level: int = 0
+        self, seg_source: str, seg_source_key: str, mip_level: int = 0, use_green_threads: bool = False
     ) -> cloudvolume.CloudVolume:
         """A helper function to create a cloudvolume client and cache it for reuse.
 
@@ -35,17 +40,24 @@ class CloudVolumeGateway:
             seg_source (str): The cloudvolume source string.
             seg_source_key (str): The cloudvolume source key name to use for caching.
             mip_level (int, optional): The MIP level to use. Defaults to 0.
+            use_green_threads (bool, optional): Whether to use green threads. Defaults to False.
 
         Returns:
             cloudvolume.CloudVolume: _description_
         """
+        if use_green_threads:
+            import gevent.monkey
+            gevent.monkey.patch_all(threads=False)
+          
         cv_client = cloudvolume.CloudVolume(
             seg_source,
             mip=mip_level,
             use_https=True,
             bounded=False,
             fill_missing=True,
+            green_threads=use_green_threads,
             lru_bytes=self._lru_bytes,
+            lru_encoding='crackle',
         )
 
         self._cv_clients[seg_source_key] = cv_client
